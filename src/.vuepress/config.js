817ed5238637aab937e3afb41574218bac2695a5
Massimo Nicolardi <nicolardi.massimo@gmail.com>
2022-02-01 09:04:55 +0100

    fix

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -38,7 +38,6 @@ module.exports = {
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-    ['meta', { name: 'viewport', content: 'with-device-width' }],
 
   ],
   markdown: {
